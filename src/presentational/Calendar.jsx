36768325a6707f995ca8fd28bdcfc500c1a08aee
Justin Winton <justin.winton@gmail.com>
2018-10-02 15:11:43 -0700

    adds table and displays time to Calendar

--- src/presentational/Calendar.jsx
+++ src/presentational/Calendar.jsx
@@ -8,16 +8,66 @@ class Calendar extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>Daily Calendar</h2>
-        9am -
-        <br />
-        10am -
-        <br />
-        ...
-        <br />
-        5pm -
-      </div>
+      <body>
+        <h2>Calender</h2>
+        <table>
+          <tr>
+            <th>Hour</th>
+            <th>Planner</th> 
+            <th>Interval Updates</th>
+          </tr>
+          <tr>
+            <td>8am</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>9am</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>10am</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>11am</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>12pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>1pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>2pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>3pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>4pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>5pm</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </body>
     );
   }
 }
