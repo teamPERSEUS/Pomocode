bffb92de21ec6f1a4c411c16f7ecc8c40241e51e
Michael Banzon <mrnbanzon@gmail.com>
2018-10-02 01:03:14 -0700

    Login with GitHub OAuth; Query basic GitHub Data.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,78 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { gitCodeURL, gitToken, gitQuery } = require('../utils/github');
 
-const { HOST, PORT } = process.env;
+const { HOST, PORT, DEV_PORT } = process.env;
 
 const app = express();
+
+const HOME = process.env.NODE_ENV !== 'production' ? `http://${HOST}:${DEV_PORT}` : '/';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '/../dist')));
+// set session in place
+app.use(
+  session({
+    secret: 'gitPomocode',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 
+// CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', `http://${HOST}:${DEV_PORT}`);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', '*');
   next();
 });
+
+// serve the front-end app. (May not need if serving from Webpack...)
+// app.use(express.static(path.join(__dirname, '/../dist')));
+
+/* --GITHUB API & AUTHENTICATION-- */
+// check if in session
+app.get('/session', (req, res) => {
+  const token = req.session.token || null;
+  console.log('Session token:', token);
+  res.send({ token });
+});
+
+// retrieve code from github
+app.get('/login', (req, res) => {
+  res.redirect(gitCodeURL);
+});
+
+// retrieve token from github
+app.get('/token', (req, res) => {
+  gitToken(req.query.code)
+    .then((token) => {
+      req.session.token = token;
+      console.log('Retrieved Token:', token);
+      res.redirect(HOME);
+    })
+    .catch(() => {
+      res.redirect(HOME);
+    });
+});
+
+// query github API v4(GraphQL)
+app.post('/query', (req, res) => {
+  gitQuery(req.body.token, req.body.query)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+/* --DB QUERY ENDPOINT(S)-- */
 
 app.listen(PORT, () => {
   console.log(`Listening on http://${HOST}:${PORT}`);
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Link } from '@reach/router';
+import axios from 'axios';
 import Timer from './presentational/Timer';
 import FileTree from './presentational/FileTree';
 import Dashboard from './presentational/Dashboard';
@@ -9,17 +10,46 @@ import DailyCharts from './presentational/DailyCharts';
 import IntervalUpdates from './presentational/IntervalUpdates';
 import Planner from './presentational/Planner';
 import WeeklyCharts from './presentational/WeeklyCharts';
+import queries from '../utils/queries';
 
+const serverPort = 'http://localhost:1337';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      token: '',
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`${serverPort}/session`, { withCredentials: true }).then(({ data }) => {
+      console.log('Data token:', data.token);
+      if (data.token !== null) {
+        this.setState({
+          token: data.token,
+        });
+      }
+    });
+  }
+
+  // example GitHub Query: retrieve array of repositories
+  getRepos(query) {
+    const { token } = this.state;
+    axios
+      .post(`${serverPort}/query`, { token, query }, { withCredentials: true })
+      .then(({ data }) => {
+        console.log(data.data.viewer.repositories.nodes);
+      });
   }
 
   render() {
+    // line 48 is to test GitHub API Query functionality
+    const { token } = this.state;
     return (
       <div className="header">
         <h1 className="logo">PomoCode</h1>
+        <a href={`${serverPort}/login`}> Login </a>
+        {token !== '' ? this.getRepos(queries.repoNames) : null}
         <h2 className="description">A pomodoro timer that enhances productivity</h2>
         <nav className="navBar">
           <Link to="/">Home</Link>
