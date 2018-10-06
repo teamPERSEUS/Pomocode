12a7a83c393522386e1f99f0b352453736f5a805
Justin Winton <justin.winton@gmail.com>
2018-10-06 15:22:48 -0700

    established HomePage component that utlilizes container component structure

---
+++ src/presentational/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomePageView from './HomePageView';
+
+class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <HomePageView />
+    );
+  }
+}
+export default HomePage;
---
+++ src/presentational/HomePage/HomePageView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePageView = () => { 
+  return (
+    <h2>HomePage</h2>
+  );
+};
+
+export default HomePageView;
