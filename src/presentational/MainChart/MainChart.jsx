292bffb4983d6e702eb8aa6f98712f85b31d695b
Justin Winton <justin.winton@gmail.com>
2018-10-06 15:18:59 -0700

    refactored component and set up file structure for testing

---
+++ src/presentational/MainChart/MainChart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainChart extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h2>MainChart</h2>
+    );
+  }
+}
+export default MainChart;
