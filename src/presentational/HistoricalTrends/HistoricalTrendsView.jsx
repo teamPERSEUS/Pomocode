05370d28e61d053131e66928517b8c870bbf3b4c
Justin Winton <justin.winton@gmail.com>
2018-10-06 17:27:27 -0700

    modified index.jsx file for refactoring components and changes to Dashboard reflect these changes too

--- src/presentational/HistoricalTrends/HistoricalTrendsView.jsx
+++ src/presentational/HistoricalTrends/HistoricalTrendsView.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const HistoricalTrendsView = () => { 
   return (
     <div>
-      <h2>HistoricalTrends</h2>
+      <h2>Historical Trends</h2>
+      <h3>Analyze your work history to determine how you are performing.</h3>
     </div>
   );
 };
