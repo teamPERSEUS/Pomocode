be91f04569d8509b1bd95ff0f08d7f96e31d05c9
Justin Winton <justin.winton@gmail.com>
2018-10-08 15:13:20 -0700

    IntervalList developed and set up for mapping data with individual intervals rendering

--- src/presentational/IntervalUpdates/IntervalList/Interval/IntervalView.jsx
+++ src/presentational/IntervalUpdates/IntervalList/Interval/IntervalView.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const IntervalView = () => { 
   return (
-    <h2>Interval</h2>
+    <div className="interval-entry">
+      <h3>Interval</h3>
+      <div>
+        {/* Analysis data here */}
+        Sed arcu lectus, rutrum vel est non, tempus accumsan
+        <br />
+        diam. In rutrum imperdiet ex, id iaculis augue facilisis non. Phasellus.
+      </div>
+    </div>
   );
 };
 
