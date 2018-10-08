5915ff54cd5f3c02a68a25103a5598198464f0b5
Justin Winton <justin.winton@gmail.com>
2018-10-08 14:38:47 -0700

    Homepage now rendering five components switching from Dashboard as default path

--- src/presentational/HomePage/HomePageView.jsx
+++ src/presentational/HomePage/HomePageView.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import MainChart from '../MainChart/MainChart';
+import IntervalUpdates from '../IntervalUpdates/IntervalUpdates';
+import IssueSelector from '../IssueSelector/IssueSelector';
+import IssueProgress from '../IssueProgress/IssueProgress';
+import SubChart from '../SubChart/SubChart';
 
 const HomePageView = () => { 
   return (
-    <h2>HomePage</h2>
+    <div>
+      <h2>HomePage</h2>
+      <MainChart />
+      <IntervalUpdates />
+      <IssueSelector />
+      <IssueProgress />
+      <SubChart />
+    </div>
   );
 };
 
