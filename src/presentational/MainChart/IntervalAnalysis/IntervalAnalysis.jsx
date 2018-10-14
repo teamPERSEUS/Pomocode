6251b7c277de4c49a94021f5248d60baa082e1ad
Fredrick Lou <fredrick.lou@gmail.com>
2018-10-14 16:03:20 -0700

    Interval details chart rendering dynamically

--- src/presentational/MainChart/IntervalAnalysis/IntervalAnalysis.jsx
+++ src/presentational/MainChart/IntervalAnalysis/IntervalAnalysis.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import c3 from 'c3';
 import '../../../../node_modules/c3/c3.css';
+import axios from 'axios';
 import IntervalAnalysisView from './IntervalAnalysisView';
 
-// const axios = require('axios');
 
 class IntervalAnalysis extends React.Component {
   constructor(props) {
@@ -15,71 +15,66 @@ class IntervalAnalysis extends React.Component {
         runningIdle: ['Running(Idle)', 3, 3, 4, 1],
         breakActive: ['Break(Active)', 1, 2, 3, 4],
         breakIdle: ['Break(Idle)', 2, 1, 4, 3],
-        groups: [
-          ['Running(Active)', 'Running(Idle)'],
-          ['Break(Active)', 'Break(Idle)'],
-        ],
-        git_id: 'Hi',
-        username: 'hellojohnny23',
-        reponame: 'Periodic Table',
-        number: 22,
-        title: 'on click table displays more info on element',
-        body: 'clicking an element in the periodic table will render a description of that element and how it interacts with other elements',
-        hoursWorked: '6hrs',
-        totalWordCount: '876',
+        groups: [['Running(Active)', 'Running(Idle)'], ['Break(Active)', 'Break(Idle)']],
+        intervalNum: 'Hi',
       },
     };
     this.updateChart = this.updateChart.bind(this);
   }
 
   componentDidMount() {
-    // this.getIssuesData();
-    this.updateChart();
+    this.getIssuesData();
   }
 
-  // getIssuesData() {
-  //   axios.get(`http://localhost:4002/api/intervalAnalysis?git_id=${this.state.git_id}`)
-  //     .then(function (response) {
-  //       console.log(response)
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error)
-  //     })
-  // }
+  getIssuesData() {
+    axios
+      .get('http://localhost:4002/api/intervalDetails', {
+        params: {
+          intervalNum: 1,
+          repoUrl: 'https://github.com/teamPERSEUS/Pomocode',
+          user: 'fredricklou523',
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.setState(
+          {
+            item: response.data,
+          },
+          () => {
+            this.updateChart();
+          },
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   updateChart() {
     const { item } = this.state;
-    if (item.git_id !== undefined) {
-      const bind = `#${item.git_id}`;
-      const columns = Object.keys(item).reduce((acc, key) => {
-        if (key !== 'groups' && Array.isArray(item[key])) {
-          acc.push(item[key]);
-        }
-        return acc;
-      }, []);
-      c3.generate({
-        bindto: bind,
-        data: {
-          x: 'x',
-          columns,
-          type: 'bar',
-          groups: item.groups,
+    c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'fileName',
+        columns: item.columns,
+        type: 'bar',
+        groups: item.groups,
+      },
+      axis: {
+        x: {
+          type: 'category',
         },
-        axis: {
-          x: {
-            type: 'category',
-          },
-          y: {
-            label: 'Time(hrs)',
-          },
+        y: {
+          label: 'Time(hrs)',
         },
-        bar: {
-          width: {
-            ratio: 0.5,
-          },
+      },
+      bar: {
+        width: {
+          ratio: 0.5,
         },
-      });
-    }
+      },
+    });
   }
 
   render() {
--- src/presentational/MainChart/IntervalAnalysis/IntervalChartView/IntervalChartView.jsx
+++ src/presentational/MainChart/IntervalAnalysis/IntervalChartView/IntervalChartView.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const IntervalChartView = ({ item }) => {
-  if (item.git_id !== undefined) {
-    return <div id={item.git_id} />;
-  }
-  return null;
-};
+const IntervalChartView = () => <div id="chart" />;
 
 export default IntervalChartView;
