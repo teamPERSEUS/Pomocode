18fc0b875a5dee43ac3c0de3747dbd5d14ca40c9
Justin Winton <justin.winton@gmail.com>
2018-10-17 13:46:41 -0700

    initial styling of header/body

--- src/presentational/Header/HeaderView.jsx
+++ src/presentational/Header/HeaderView.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import Login from './Login/Login';
-import Navbar from './Navbar/Navbar';
 import PerseusTomato from '../../img/PerseusTomato.png';
 
 const HeaderView = ({ logout }) => (
   <div className="header">
     <Link to="/">
-      <img className="logo" src={PerseusTomato} style={{ width: 70, height: 65 }} alt="Perseus Logo" />
+      <img className="logo" src={PerseusTomato} style={{ width: 60, height: 50 }} alt="Perseus Logo" />
     </Link>
-    <button className="logout-button" type="button" onClick={logout}>Logout</button>
     <h3 className="header-title">Pomocode</h3>
-    <Login />
-    <Navbar />
+    <button className="logout-button" type="button" onClick={logout}>Logout</button>
   </div>
 );
 
--- src/presentational/HistoricalTrends/HistoricalTrendsView.jsx
+++ src/presentational/HistoricalTrends/HistoricalTrendsView.jsx
@@ -3,7 +3,7 @@ import Historical from '../../img/Historical.png';
 
 const HistoricalTrendsView = () => { 
   return (
-    <div className="sub-chart">
+    <div className="historical-trends">
       <h3>Historical Trends</h3>
       <img src={Historical} style={{ width: 450, height: 300 }} alt="Historical Trends Table" />
     </div>
