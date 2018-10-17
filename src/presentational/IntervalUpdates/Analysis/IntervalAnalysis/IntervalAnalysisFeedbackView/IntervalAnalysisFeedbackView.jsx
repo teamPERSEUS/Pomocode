23fc9f2e47c6ae12bcf2953eaf9bd72686021432
Michael Banzon <mrnbanzon@gmail.com>
2018-10-16 22:00:54 -0700

    Formatted Interval Analysis to data coming in.

--- src/presentational/IntervalUpdates/Analysis/IntervalAnalysis/IntervalAnalysisFeedbackView/IntervalAnalysisFeedbackView.jsx
+++ src/presentational/IntervalUpdates/Analysis/IntervalAnalysis/IntervalAnalysisFeedbackView/IntervalAnalysisFeedbackView.jsx
@@ -3,16 +3,7 @@ import React from 'react';
 const IntervalAnalysisFeedbackView = ({ item }) => (
   <div>
     <h3>Interval Feedback</h3>
-    <p>
-      You have worked
-      {' '}
-      {item.hoursWorked}
-      {' '}
-      on this interval and your word count is
-      {' '}
-      {item.totalWordCount}
-      .
-    </p>
+    <p>{item.feedback}</p>
   </div>
 );
 
