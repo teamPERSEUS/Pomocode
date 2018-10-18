e0e95f711a2ffc3d2d7a36870ddf55626f79c5ad
Michael Banzon <mrnbanzon@gmail.com>
2018-10-17 22:30:46 -0700

    Initial configuration for heroku deployment.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-// const path = require('path');
+const path = require('path');
 const axios = require('axios');
 const express = require('express');
 const session = require('express-session');
@@ -7,13 +7,14 @@ const bodyParser = require('body-parser');
 const { gitCodeURL, gitToken } = require('../utils/github');
 
 const {
-  HOST, PORT, WEB_PORT, GIT_PORT,
+  HOST, PORT, WEB_PORT, GIT_PORT, PLANNER_SERVICE,
 } = process.env;
 
 const app = express();
 
 const HOME = process.env.NODE_ENV !== 'production' ? `http://${HOST}:${WEB_PORT}` : '/';
-const GITSERVICE = process.env.NODE_ENV !== 'production' ? `http://${HOST}:${GIT_PORT}` : '/';
+const CORS = process.env.NODE_ENV !== 'production' ? `http://${HOST}:${WEB_PORT}` : '*';
+const PLANSERVICE = process.env.NODE_ENV !== 'production' ? `http://${HOST}:${GIT_PORT}` : PLANNER_SERVICE;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,15 +31,17 @@ app.use(
 
 // CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${HOME}`);
+  res.header('Access-Control-Allow-Origin', `${CORS}`);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', '*');
   next();
 });
 
-// serve the front-end app. (May not need if serving from Webpack...)
-// app.use(express.static(path.join(__dirname, '/../dist')));
+// serve the front-end app if in production (in heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/../dist')));
+}
 
 /* --GITHUB API & AUTHENTICATION-- */
 // check if in session
@@ -64,7 +67,7 @@ app.get('/authorize', (req, res) => {
   gitToken(req.query.code)
     .then(({ data }) => {
       req.session.token = data.access_token;
-      return axios.post(`${GITSERVICE}/login`, { token: data.access_token });
+      return axios.post(`${PLANSERVICE}/login`, { token: data.access_token });
     })
     .then((user) => {
       req.session.user = user.data.login;
@@ -79,5 +82,5 @@ app.get('/authorize', (req, res) => {
 /* --DB QUERY ENDPOINT(S)-- */
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://${HOST}:${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
